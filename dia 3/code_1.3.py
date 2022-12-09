d4b0cdb28b466f8146d6e2f7e116c231222f20b4
hallzyx <hallinxsl7@gmail.com>
2022-12-09 12:59:46 -0500

    Probando iterativas y funciones con listas

--- dia 3/code_1.3.py
+++ dia 3/code_1.3.py
@@ -13,3 +13,23 @@ print("----------")
 
 for indice in range(3):
     print(notas[indice])
+
+
+
+series=[]
+def new_series(nombre, tipo, genero ):
+    series.append({
+            'Nombre':nombre,
+            'Tipo': tipo,
+            'Genero': genero,
+        })
+
+new_series("PLAYER","manhwa","shonen")
+print(series)
+
+
+nom=[]
+nom.append("numeroxd")
+print(nom)
+nom.append({"numero":1})
+print(nom)
