b278f4e4f46bb3e7067c5be556301003cabbb277
hallzyx <hallinxsl7@gmail.com>
2022-12-09 12:45:13 -0500

    Usando iteradores

---
+++ dia 3/code_1.3.py
@@ -0,0 +1,15 @@
+notas=[10,20,16,8,6,1]
+
+
+for nota in notas:
+    print(nota)
+
+
+print("----------")
+
+print(notas[0:3])  
+
+print("----------")
+
+for indice in range(3):
+    print(notas[indice])
