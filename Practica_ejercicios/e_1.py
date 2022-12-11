dfca6a7bf83ba5192633ebd6c5ba1609da149189
hallzyx <hallinxsl7@gmail.com>
2022-12-11 09:01:33 -0500

    Mejora del ejercicio 1

--- Practica_ejercicios/e_1.py
+++ Practica_ejercicios/e_1.py
@@ -1,7 +1,9 @@
 nombre=input("Introdusca su nombre: ")
 edad=int(input("Introdusca su edad: "))
-
+anio=int(input("Cual es el año actual: "))
 dif=100-edad
 
+anio_2=anio+dif
 
-print("El usuario {} cumplirá 100 años en {} años".format(nombre,dif))
+print("El usuario {} cumplirá 100 años en {} años, es decir en el año {}".format(nombre,dif,anio_2))
+
