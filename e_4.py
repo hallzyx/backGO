e2057cec6866b66abe9e0cdd35aa891737194773
hallzyx <hallinxsl7@gmail.com>
2022-12-11 12:37:10 -0500

    Ejercicio 4 resuelto de diferentes maneras

---
+++ e_4.py
@@ -0,0 +1,27 @@
+##Primera forma de resolver el problema
+# n=int(input("Introdusca el numero deseado: "))
+# i=1
+# div=[]
+# while True:
+#     if(i==n+1):    
+#        break
+#     if(n%i==0):
+#         div.append(i)
+#     i+=1
+
+# print(div)
+
+
+##Segunda forma de resolver el problema
+
+# n=int(input("Introdusca el numero deseado: "))
+
+# lista=list(range(1,n+1))
+
+# div=[]
+
+# for numero in lista:
+#     if (n%numero==0):
+#         div.append(numero)
+
+# print(div)
